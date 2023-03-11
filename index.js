eb705f87bd10c6d0491008cfe9d54d0a88be2ccf
marcojurupe <marco.jurupe@gmail.com>
2023-03-11 12:11:39 -0500

    Modified file

---
+++ index.js
@@ -0,0 +1,18 @@
+let name = "Gracie";
+let language = "Python";
+
+function introduction(name) {  
+   return `Hi, my name is ${name}.`;
+}
+  
+function introductionWithLanguage(name, language) {
+   return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+} 
+
+function introductionWithLanguageOptional(name, language) {
+   return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+} 
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+   return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+} 
